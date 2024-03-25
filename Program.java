0433e02e3cd9ac2ec84044f899da1753bc1be892
BerindeanuDavid <david.berindeanu@emanuel.ro>
2024-03-25 11:40:41 +0200

    schimbari conform cerintei corectate

--- Program.java
+++ Program.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
+import java.util.HashSet;
 public class Program {
     public static void main(String[] args)
     {
@@ -12,23 +13,40 @@ public class Program {
             Interval.writer=new FileWriter("statistica.dat");
             //
             ArrayList<Double> doubleArray=new ArrayList<>();
+            HashSet<String> set= new HashSet<>();
             String line1=reader1.readLine();
+            boolean invalid=false;
             while(line1!=null)
             {
                 doubleArray.add(new Double(line1));
                 line1=reader1.readLine();
-            }
-            String line2=reader2.readLine();
-            while(line2!=null)
-            {
-                String[] intervalParameters=line2.split(",");
-                Interval interval=new Interval(intervalParameters[0].substring(1,intervalParameters[0].length()),intervalParameters[1].substring(0,intervalParameters[1].length()-1));
-                for(Double d:doubleArray)
+                if(set.contains(line1))
                 {
-                    interval.testare(d);
+                    invalid=true;
                 }
-                interval.rezultat();
-                line2=reader2.readLine();
+                else
+                {
+                    set.add(line1);
+                }
+            }
+            if(invalid)
+            {
+                System.out.println("Numerele trebuie sa fie introduse doar o data.");
+            }
+            else
+            {
+                String line2=reader2.readLine();
+                while(line2!=null)
+                {
+                    String[] intervalParameters=line2.split(",");
+                    Interval interval=new Interval(intervalParameters[0].substring(1,intervalParameters[0].length()),intervalParameters[1].substring(0,intervalParameters[1].length()-1));
+                    for(Double d:doubleArray)
+                    {
+                        interval.testare(d);
+                    }
+                    interval.rezultat();
+                    line2=reader2.readLine();
+                }
             }
             reader1.close();
             reader2.close();
